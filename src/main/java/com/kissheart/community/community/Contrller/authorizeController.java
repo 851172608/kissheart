ac04c7e11ea5e3ce589e9383a734fdb9957a2c26
851172608 <851172608@qq.com>
2021-01-27 17:54:14 +0800

    hello

--- src/main/java/com/kissheart/community/community/Contrller/authorizeController.java
+++ src/main/java/com/kissheart/community/community/Contrller/authorizeController.java
@@ -1,5 +1,6 @@
 package com.kissheart.community.community.Contrller;
 
+import com.kissheart.community.community.dto.GithubUser;
 import com.kissheart.community.community.dto.accessTokenDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.kissheart.community.community.Provider.githubProvider;
+
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class authorizeController {
     @Autowired
@@ -22,18 +26,30 @@ public class authorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
 
         accessTokenDto accessTokenDto = new accessTokenDto();
-
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
         accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
 
-        githubProvider.getAccessToken(new accessTokenDto());
-        return "index";
 
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+
+        System.out.println(accessTokenDto);
+        System.out.println(accessToken);
+        System.out.println(user.getName());
+        if(user != null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }
+        else {
+            return "redirect:/";
+        }
     }
 }
